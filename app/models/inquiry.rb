1de520eda883d589e8c8b72af8ec6adae27026b7
YutakaTW <mini.naruchan.ningyou@gmail.com>
2018-09-29 16:57:17 +0800

    translate

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -3,8 +3,8 @@ class Inquiry
 
   attr_accessor :name, :email, :subject, :message
 
-  validates :name, :presence => {:message => 'Please input your name'}
+  validates :name, :presence => {message: :namealert}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, :format => {with: VALID_EMAIL_REGEX, :message => 'Please input your email address'}
-  validates :message, :presence => {:message => 'Please input your inquiry'}
+  validates :email, :format => {with: VALID_EMAIL_REGEX, message: :emailalert}
+  validates :message, :presence => {message: :inquiryalert}
 end
